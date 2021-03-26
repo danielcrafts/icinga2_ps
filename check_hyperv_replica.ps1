3ecddb7836412e8a0b305ca8cedb987f277f0f46
Daniel Crafts <dc@bbtronic.de>
2021-03-26 22:50:33 +0100

    Create check_hyperv_replica.ps1

---
+++ check_hyperv_replica.ps1
@@ -0,0 +1,20 @@
+	# Checks if RebootRequired key exists, if so returns a warning.
+	# This key is deleted upon a successful reboot.
+	# This may indicate that Windows patching has taken place, without a reboot.
+		 
+	# Checks if RebootRequired reg path exists
+	$value = (Get-VMReplication –ReplicationState Error | measure).count
+	 
+	# If path does not exist, return OK status
+	if ($value -match "0") {
+	echo "OK - No Replication Error's"
+	$returnCode=0
+	}
+	 
+	# Else return Critical status
+	else {
+	echo "CRITICAL - $value Replication Error's"
+	$returnCode=1
+	}
+	 
+	exit ($returnCode)
