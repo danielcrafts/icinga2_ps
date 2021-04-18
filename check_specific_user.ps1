0b197de10b54ebf4476e1f8a3d8c6eee5105d6ad
danielcrafts <55715957+danielcrafts@users.noreply.github.com>
2021-04-19 01:11:43 +0200

    fixed path

--- check_specific_user.ps1
+++ check_specific_user.ps1
@@ -1,6 +1,16 @@
 $value = Get-WmiObject -Class Win32_ComputerSystem | Select-Object UserName
 $user = Get-Content .\specific_user.conf
 
+#Path Function
+function Get-ScriptDirectory {
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+ 
+#Fixing Admin Path
+$runningpath = Get-ScriptDirectory
+cd $runningpath
+
 	if ($value -match $user) {
 	echo "OK $value"
 	}
