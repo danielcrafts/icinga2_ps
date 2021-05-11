cd4b4af0c95756d5e968eb9ada048fac80053c7b
Daniel Crafts <dc@bbtronic.de>
2021-05-11 21:42:34 +0200

    specify critical output

--- check_disk_error.ps1
+++ check_disk_error.ps1
@@ -1,20 +1,28 @@
 $disk = Get-PhysicalDIsk | Get-StorageReliabilityCounter |  Select-Object DeviceID, ReadErrorsTotal, WriteErrorsTotal, Wear, Temperature
 $state = 0
+$read = ""
+$write = ""
+$wear = ""
+$temp = ""
 
 if ($disk.ReadErrorsTotal -gt "0"){
 $state = 1
+$read = ReadErrorsTotal
 }
 
 if ($disk.WriteErrorsTotal -gt "0"){
 $state = 1
+$write = WriteErrorsTotal
 }
 
 if ($disk.Wear -gt "0"){
 $state = 1
+$wear = Wear
 }
 
 if (($disk.Temperature -gt "64") -and ($disk.Temperature -lt "240")){
 $state = 1
+$temp = Temperature
 }
 
 	if ($state -match "0") {
@@ -23,8 +31,9 @@ $state = 1
 	}
 
     if ($state -match "1") {
-    echo "Critical - $disk"
-	$returnCode=2
+    echo "Critical - $read $write $wear $temp"
+	$returnCode=1
     }
 
 	exit ($returnCode)
+
