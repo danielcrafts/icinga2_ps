7a6cddaabb082939d0b73855b88f813e69576890
danielcrafts <55715957+danielcrafts@users.noreply.github.com>
2021-05-15 00:38:32 +0200

    changed logfile

--- check_puppetrun.ps1
+++ check_puppetrun.ps1
@@ -1,14 +1,15 @@
-$filename = "C:\ProgramData\PuppetLabs\puppet\cache\state\last_run_summary.yaml"
-$value = Get-Content $filename |Select-String -Pattern 'failure'
+$filename = "C:\ProgramData\PuppetLabs\puppet\cache\state\last_run_report.yaml"
+$value = Get-Content $filename |Select-String -Pattern 'status:'
 
-	if ($value -match "0") {
-	echo "OK $value"
+	if ($value -match "failed") {
+    echo "CRITICAL - last run failed "
+	$returnCode=2
 	}
 	 
 	# Else return CRITICAL status
 	else {
-	echo "CRITICAL - $value "
-	$returnCode=2
+	echo "OK - no error"
+	$returnCode=0
 	}
 	 
 	exit ($returnCode)
