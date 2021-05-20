805d0c05b55aa218557cfba733e9218aa2c6589d
Daniel Crafts <dc@bbtronic.de>
2021-05-21 00:06:09 +0200

    adding missing cleanup and output

--- check_altaro.ps1
+++ check_altaro.ps1
@@ -7,27 +7,50 @@ $pattern = "Failed"
 )
 $count = 0
 $Backups = Get-WinEvent -FilterHashTable @{ProviderName="Altaro VM Backup";LogName="Application";ID=$event;StartTime=(get-date).AddHours(-24)} 
+$socks = ls C:\tmp\altaro\
 
-foreach ($Backup in $Backups)
-{
+#cleanup sock and lock here
+foreach ($file in $socks.name){
+	$fileage = Test-Path $file -OlderThan (Get-Date).AddDays(-"0").AddHours(-"25").AddMinutes(-"0")
+	if ($fileage -match "True") {
+		Remove-Item -itemtype "file" -path "C:\tmp\altaro\$file" -Force
+		}
+	}
+		
+foreach ($Backup in $Backups){
+	# Format VM Name
 	$server = $Backup.Message -split "`n" | Where-Object {$_ -match 'Name:'}
     $fail = $server.Split(':')[1]
 	$fail = $fail -replace '\s',''
-    if ($Backup.Message -match $pattern) {$count++}
-	$sock = "C:\tmp\$fail.sock"
-	$sock2 = "C:\tmp\$fail.lock"
+
+	#Specific Vars and sock
+	$sock = "C:\tmp\altaro\$fail.sock"
+	$sock2 = "C:\tmp\altaro\$fail.lock"
+		if(test-path $sock -match "False" ) {
+		New-Item -itemtype "file" -path "$sock" -Force
+		}
+	$sockage = Test-Path $sock -OlderThan (Get-Date).AddDays(-"0").AddHours(-"25").AddMinutes(-"0")
 	
-	if ((test-path "$sock") -match "True") {
-		$sockage = Test-Path $sock -OlderThan (Get-Date).AddDays(-"0").AddHours(-"49").AddMinutes(-"0")
-		}
-	if ($sockage -match "true"){
-		Remove-Item $sock -Force
-		}
-	if ((test-path "$sock") -match "True") {
+	if ($sockage -match "True") {
 		New-Item -itemtype "file" -path "$sock2" -Force
 		$count++
 		}
 
-}
+	}
 
-Write-Host " 2 $pattern Backup for $count Machines in last 48 hours"
+#mesure sock for warning
+$sockcount = (ls C:\tmp\altaro\*.sock | measure).count
+#mesure lock for crit
+$lockcount = (ls C:\tmp\altaro\*.lock | measure).count
+
+if ($lockcount -gt "0") {
+	Write-Host " Critical - 2 $pattern Backup for $lockcount Machines in last 48 hours"
+	$returnCode=2
+	}
+
+if (($lockcount -gt "0") -and ($lockcount -gt "0")) {
+	Write-Host " Warning - 1 $pattern Backup for $sockcount Machines in last 24 hours"
+	$returnCode=1
+	}
+	
+	exit ($returnCode)
